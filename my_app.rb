7d0fb4e963151e466913c3265550bd54dd0cce6d
Javier Arce <javierarce@vizzuality.com>
2011-05-26 18:55:31 +0200

    Fix

--- my_app.rb
+++ my_app.rb
@@ -25,7 +25,7 @@ end
 
 def is_authorized?
   twitter_cookie = request.cookies["twitter_anywhere_identity"]
-  return false if twitter_cookie.nil? or twitter.cookie == ""
+  return false if twitter_cookie.nil? or twitter_cookie == ""
 
   cookie  = twitter_cookie.split(":")
 
